1dbff3f3bd23dd6a2a20ce0b484a50077b5158fd
MaciejDulewiczGU <151458358+MaciejDulewiczGU@users.noreply.github.com>
2025-08-02 23:04:32 +0200

    Add files via upload

---
+++ Correlation_analysis_GeoMX_and_MALDI_LP_MODE.R
@@ -0,0 +1,16 @@
+# The general code for spectail transcriptomics data and correlation with MALDI MSI:
+
+#corrFunc <- function(var1, var2, data1) {
+#  if (!is.numeric(data1[[var1]]) || !is.numeric(data1[[var2]])) {
+#    warning("Both variables must be numeric for correlation calculation.")
+#    return(NULL)
+#  }
+  
+#  result <- cor.test(data1[[var1]], data1[[var2]], na.rm = TRUE)
+#  data.frame(var1, var2, result[c("estimate", "p.value", "statistic", "method")], 
+#             stringsAsFactors = FALSE)
+#}
+
+
+# corrs_trans_df_AB_for_correlation_norm_Quant_10 = do.call(rbind, mapply(corrFunc, vars[,1], vars[,2], 
+#                                                                        MoreArgs=list(data=trans_df_AB_for_correlation_norm_Quant_10), SIMPLIFY=FALSE))
